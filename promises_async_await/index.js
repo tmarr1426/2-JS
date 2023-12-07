978501527bf29130b2fa6aaf08f732ff3095a4b8
tmarr1426 <tmarr1426@gmail.com>
2023-12-06 19:53:51 -0600

    Promise Async Await notes

---
+++ promises_async_await/index.js
@@ -0,0 +1,108 @@
+/*
+?   Promises
+    * An object that may produce a singular value in the future
+
+    ? Three States
+        * Pending (unfinished)
+        * Fulfilled (Completed with a value)
+        * Rejected (error)
+        
+        * Promises allow other code to execute without having to wait for their completion
+*/
+
+function standard() {
+    return "No promises here";
+}
+
+console.log(standard());
+
+function getData () {
+    setTimeout(() => {
+        console.log("running after 3 seconds");
+        return "Data";
+    }, 3000);
+}
+
+let data = getData();
+console.log(data); //! ERROR data is undefined for 3 seconds as it takes 3 seconds to return
+
+function promiseFx(value){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(value) {
+                resolve("data in promise");
+            } else {
+                reject("Cannot complete request");
+            } 
+        }, 3000);
+    });
+};
+
+//console.log(promiseFx());//! PENDING STATE
+
+//? In order to get tangible data out of the Promise object, we MUST resolve it
+//. then . catch syntax below
+
+promiseFx(5)
+.then((msg) => console.log(msg))
+.catch((err)=>console.log("Hey you hit an error"));
+
+/*
+?   Asynchronous Functions
+    * Introduced in ES7
+    * Alternative way to write promises
+    * A function that returns a promise object
+    * Allows us to resolve or reject a promise
+
+    ? Syntax
+    
+    async function myFx() {} //? Traditional Function
+    const myFx = async function () {} //? Function Expression
+    const myFx = () => {} //? Arrow Function
+*/
+
+async function asyncFx(){
+    return "Hello Learners";
+}
+
+console.log(asyncFx());
+
+//? How do we get values out of async functions
+
+// 1. Use a resolver
+asyncFx().then((val) => console.log(val));
+
+// 2. Use await keyword
+async function start() {
+    const results = await asyncFx();
+    console.log(val);
+}
+
+start();
+
+let networkStatus = false;
+
+function promiseEx(networkConnection) {
+    return new Promise ((resolve, reject) => {
+        setTimeout(()=> {
+            if(networkConnection) {
+                resolve("This is data, woot woot!!");
+            } else {
+                reject("Sad Day. Please check network connection.")
+            };
+        }, 5000);
+    });
+}
+
+//? Async
+
+async function handlePromiseAsync() {
+    try {
+        console.log(await promiseEx());
+        console.log("Data received");
+    } catch (err) {
+        console.log(err);
+    };
+}
+
+handlePromiseAsync();
--- scope_hoisting/index.js
+++ scope_hoisting/index.js
@@ -42,6 +42,19 @@ let x = 5;
 
 // console.log(innerScopedVariable); //! ERROR: innerScopedVariable is not defined. Cannot reach inner nestings when outside.
 
+//? Var vs Let in scope
+
+{
+    let x = 5; 
+}
+
+//console.log(x); //! ERROR undefined
+
+{
+    var y = 5;
+}
+
+console.log(y); //! Var can be read
 /*
 ?   Function Scope
     * Accessible only inside of the function
@@ -71,23 +84,64 @@ scope();
     * Inner fx scope can access outer function scope
 */
 
+function outerFx () {
+    let ofxv = "Outer fx Scope Variable";
 
+    function innerFx() {
+        let ifxv = "Inner fx Scope Variable";
+        return ifxv;
+    }
 
+    return innerFx ();
+}
 
+console.log(outerFx ());
 
+/*
+?   Hoisting
+    * JS parsing engine reads top to bottom, left to right
+    * JS interpreter runs the code twice
+        - First, it looks for VARiables and fx declarations and hoists them
+        - It allocates memory space for those declarations
+        - It then executes the code line-by-line
+*/
 
+//console.log(myDog);//! ERROR cannot read a variable below it.
 
+let myDog = "Spaniel";
+console.log(myDog);
 
+var age = 32;
+console.log(age);
 
+x (); // Traditional functions get hoisted. Able to call them before they are declared
 
+function x () {
+    console.log("fucntion x executed")
+}
 
+//y (); //! ERROR Function expressions do NOT get hoisted above everything.
 
+let y = function () {
+    console.log("function expression")
+};
 
+y ();
 
+function run() {
+    var foo = "foo";
+    let bar = "bar";
+    var fxVariable = "test";
+    console.log(foo, bar, fxVariable);
 
+    {
+        var moo = "moo";
+        let baz = "baz";
+        console.log(moo, baz);
+    }
 
+    //console.log(baz); //! ERROR undefined, let does not get hoisted
+    console.log(moo); // Able to print due to moo being a VAR
+};
 
-
-
-
-
+run();
